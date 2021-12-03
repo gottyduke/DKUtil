7125e3f1280af5327bef14c75218e65c30e8754e
DK <gottyduke@hotmail.com>
2021-12-03 15:42:14 -0600

    AE formatting

--- test/PCH.h
+++ test/PCH.h
@@ -1,13 +1,19 @@
 #pragma once
 
 
-
+// CommonLib
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 
+using namespace std::literals;
+
+// Version
+#include "Version.h"
+
+// DKUtil
 #include "DKUtil/Logger.hpp"
 
-#include "Version.h"
+// Include
 
 
 #define DLLEXPORT __declspec(dllexport)
--- test/PluginTest.cpp
+++ test/PluginTest.cpp
@@ -2,6 +2,24 @@
 #include "HookTest.h"
 
 
+#if ANNIVERSARY_EDITION
+
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []()
+{
+	SKSE::PluginVersionData data{};
+
+	data.PluginVersion(Version::MAJOR);
+	data.PluginName(Version::NAME);
+	data.AuthorName("Dropkicker"sv);
+
+	data.CompatibleVersions({ SKSE::RUNTIME_LATEST });
+	data.UsesAddressLibrary(true);
+
+	return data;
+}();
+
+#else
+
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
 	DKUtil::Logger::Init(Version::PROJECT, Version::NAME);
@@ -10,25 +28,42 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 	a_info->name = Version::PROJECT.data();
 	a_info->version = Version::MAJOR;
 
+	if (a_skse->IsEditor()) {
+		ERROR("Loaded in editor, marking as incompatible"sv);
+		return false;
+	}
+
 	const auto ver = a_skse->RuntimeVersion();
-	if (ver < SKSE::RUNTIME_1_5_39)
-	{
-		ERROR("Unsupported runtime version {}", ver.string());
+	if (ver < SKSE::RUNTIME_1_5_39) {
+		ERROR("Unable to load this plugin, incompatible runtime version!\nExpected: Newer than 1-5-39-0 (A.K.A Special Edition)\nDetected: {}", ver.string());
 		return false;
 	}
 
 	return true;
 }
 
+#endif
+
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
+#if ANNIVERSARY_EDITION
+
+	DKUtil::Logger::Init(Version::PROJECT, Version::NAME);
+
+	if (REL::Module::get().version() < SKSE::RUNTIME_1_6_317) {
+		ERROR("Unable to load this plugin, incompatible runtime version!\nExpected: Newer than 1-6-317-0 (A.K.A Anniversary Edition)\nDetected: {}", REL::Module::get().version().string());
+		return false;
+	}
+
+#endif
+
 	INFO("{} v{} loaded", Version::PROJECT, Version::NAME);
 
-	Init(a_skse);
+	SKSE::Init(a_skse);
 
 	Test::Config::Load();
 	Test::Hook::Install();
-
+	
 	return true;
 }
