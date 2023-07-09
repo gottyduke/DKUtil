ff990a4cded65a7187a50c25ad363d83f7f35b36
DK <gottyduke@hotmail.com>
2023-07-10 07:20:11 +0800

    separate self contained `Utility` headers

--- test/UtilityTest.h
+++ test/UtilityTest.h
@@ -11,6 +11,8 @@
 
 namespace Test::Utility
 {
+	using namespace DKUtil::Alias;
+
 	namespace Enum
 	{
 		enum class ContiguousFlag ENUM_TYPE
@@ -104,7 +106,70 @@ namespace Test::Utility
 			R14 = 16,
 			R15 = 17,
 		};
-	} // namespace Enum
+
+		void TestEnum() noexcept
+		{
+			// concept-restraint auto ctor
+			dku::enumeration<ContiguousValue UNDERLYING> cValues{ 0, 2, 4, 5, 9, 15 };
+			dku::enumeration<SparseValue UNDERLYING> sValues{ 0, 2, 4, 5, 9, 15 };
+			dku::enumeration<ContiguousFlag UNDERLYING> cFlags{ 0, 2, 4, 5, 9, 15 };
+			dku::enumeration<SparseFlag UNDERLYING> sFlags{ SparseFlag::NONE, SparseFlag::RCX, SparseFlag::RBX, SparseFlag::RSI, SparseFlag::R9, SparseFlag::R14 };
+
+			// static reflections
+			// 1) check for value-type enum reflection
+			// 2) check for flag-type enum reflection
+			// 3) check for mixed sparse enum reflection
+			// 4) check for mixed contiguous enum reflection
+			// 5) direct invocation between raw underlying value and enum value
+			INFO("Reflecting enum value names");
+			for (auto i : std::views::iota(0, 17)) {
+				INFO("{}{} {}", cValues.is_flag() ? "1<<" : "", i, cValues.to_string(static_cast<ContiguousValue>(i)));
+				INFO("{}{} {}", sValues.is_flag() ? "1<<" : "", i, sValues.to_string(i));
+				INFO("{}{} {}", cFlags.is_flag() ? "1<<" : "", i, cFlags.to_string(i));
+				INFO("{}{} {}", sFlags.is_flag() ? "1<<" : "", i, sFlags.to_string(i));
+			}
+
+			// 5) check for enum class name reflection
+			INFO("Enum name:\n{}\n{}\n{}\n{}", cValues.enum_name(), sValues.enum_name(), cFlags.enum_name(), sFlags.enum_name());
+			// 6) check for enum underlying type name
+			INFO("Type name:\n{}\n{}\n{}\n{}", cValues.type_name(), sValues.type_name(), cFlags.type_name(), sFlags.type_name());
+
+			// 7) value range iterator
+			INFO("Iterating by value_range");
+			for (const auto e : cValues.value_range(ContiguousValue::NONE, ContiguousValue::R15)) {
+				INFO("{}", cValues.to_string(e));
+			}
+
+			INFO("Iterating by flag_range");
+			for (const auto e : cFlags.flag_range(ContiguousFlag::NONE, ContiguousFlag::R15)) {
+				INFO("{}", cFlags.to_string(e));
+			}
+
+			// 8) string to enum cast
+			std::string nameStr1{ "rsi" };
+			std::string nameStr2{ "RaX" };
+			std::string nameStr3{ "Rdx" };
+
+			auto nameEnum1 = cValues.from_string(nameStr1);
+			auto nameEnum2 = cValues.from_string(nameStr2);
+			auto nameEnum3 = cValues.from_string(nameStr3);
+
+			INFO("String {} -> Enum {}", nameStr1, std::to_underlying(nameEnum1.value()));
+			INFO("String {} -> Enum {}", nameStr2, std::to_underlying(nameEnum2.value()));
+			INFO("String {} -> Enum {}", nameStr3, std::to_underlying(nameEnum3.value()));
+			ContiguousValue Enum1 = nameEnum1.value();
+		}
+
+		void TestEnumBitwidth() noexcept
+		{
+			auto& sf = dku::static_enum<SparseFlag>();
+			auto& cf = dku::static_enum<ContiguousFlag>();
+
+			auto str = "RDX";
+			auto strEnum = sf.from_string(str);
+			INFO("{} {}", dku::print_enum(strEnum.value_or(SparseFlag::RAX)), std::to_underlying(strEnum.value_or(SparseFlag::RAX)));
+		}
+	}  // namespace Enum
 
 	consteval void TestNumbers() noexcept
 	{
@@ -123,81 +188,53 @@ namespace Test::Utility
 
 	void TestModel() noexcept
 	{
-
+		// enumeration tests
 	}
 
 
 	void TestString() noexcept
 	{
+		auto str = "D:\\WorkSpace\\SKSEPlugins\\Plugins\\EnderalHeroMenu\\src\\LoadGame.cpp:36"sv;
+		auto pat = "\\"sv;
+		auto rep = "|"sv;
+
+		// 1) replace_nth
+		INFO("replace all \\ with |");
+		INFO(dku::string::replace_nth_occurrence(str, 0, pat, rep));
+		
+		INFO("remove all \\");
+		INFO(dku::string::replace_nth_occurrence(str, 0, pat));
+
+		INFO("replace 2nd \\ with |");
+		INFO(dku::string::replace_nth_occurrence(str, 2, pat, rep));
+
+		INFO("replace 2nd to last \\ with |");
+		INFO(dku::string::replace_nth_occurrence(str, -2, pat, rep));
+
+		INFO("replace 100th \\ with | (out of bound)");
+		INFO(dku::string::replace_nth_occurrence(str, 100, pat, rep));
+
+		
+		// 2) split, join
 		constexpr std::string_view words{ "DKUtil|TestUsage|Schema|String 002| That " };
-		auto token = DKUtil::string::split(words, "|");
+		auto token = dku::string::split(words, "|");
 
 		for (auto& t : token) {
 			INFO(t);
 		}
+
+		INFO(dku::string::join(token, "|"));
 	}
 
-
-	void TestEnum() noexcept
-	{
-		using namespace Enum;
-
-		// concept-restraint auto ctor
-		dku::enumeration<ContiguousValue UNDERLYING> cValues{ 0, 2, 4, 5, 9, 15 };
-		dku::enumeration<SparseValue UNDERLYING> sValues{ 0, 2, 4, 5, 9, 15 };
-		dku::enumeration<ContiguousFlag UNDERLYING> cFlags{ 0, 2, 4, 5, 9, 15 };
-		dku::enumeration<SparseFlag UNDERLYING> sFlags{ SparseFlag::NONE, SparseFlag::RCX, SparseFlag::RBX, SparseFlag::RSI, SparseFlag::R9, SparseFlag::R14 };
-		
-		// static reflections
-		// 1) check for value-type enum reflection
-		// 2) check for flag-type enum reflection
-		// 3) check for mixed sparse enum reflection
-		// 4) check for mixed contiguous enum reflection
-		// 5) direct invocation between raw underlying value and enum value
-		INFO("Reflecting enum value names");
-		for (auto i : std::views::iota(0, 17)) {
-			INFO("{}{} {}", cValues.is_flag() ? "1<<" : "", i, cValues.to_string(static_cast<ContiguousValue>(i)));
-			INFO("{}{} {}", sValues.is_flag() ? "1<<" : "", i, sValues.to_string(i));
-			INFO("{}{} {}", cFlags.is_flag() ? "1<<" : "", i, cFlags.to_string(i));
-			INFO("{}{} {}", sFlags.is_flag() ? "1<<" : "", i, sFlags.to_string(i));
-		}
-
-		// 5) check for enum class name reflection
-		INFO("Enum name:\n{}\n{}\n{}\n{}", cValues.enum_name(), sValues.enum_name(), cFlags.enum_name(), sFlags.enum_name());
-		// 6) check for enum underlying type name
-		INFO("Type name:\n{}\n{}\n{}\n{}", cValues.type_name(), sValues.type_name(), cFlags.type_name(), sFlags.type_name());
-		
-		// 7) value range iterator
-		INFO("Iterating by value_range");
-		for (const auto e : cValues.value_range(ContiguousValue::NONE, ContiguousValue::R15)) {
-			INFO("{}", cValues.to_string(e));
-		}
-
-		INFO("Iterating by flag_range");
-		for (const auto e : cFlags.flag_range(ContiguousFlag::NONE, ContiguousFlag::R15)) {
-			INFO("{}", cFlags.to_string(e));
-		}
-		
-		// 8) string to enum cast
-		std::string nameStr1{ "rsi" };
-		std::string nameStr2{ "RaX" };
-		std::string nameStr3{ "Rdx" };
-
-		auto nameEnum1 = cValues.from_string(nameStr1);
-		auto nameEnum2 = cValues.from_string(nameStr2);
-		auto nameEnum3 = cValues.from_string(nameStr3);
-
-		INFO("String {} -> Enum {}", nameStr1, std::to_underlying(nameEnum1.value()));
-		INFO("String {} -> Enum {}", nameStr2, std::to_underlying(nameEnum2.value()));
-		INFO("String {} -> Enum {}", nameStr3, std::to_underlying(nameEnum3.value()));
-		ContiguousValue Enum1 = nameEnum1.value();
-	}
 
 	void Run() noexcept
 	{
 		TestNumbers();
 		TestModel();
+		
 		TestString();
-		TestEnum();
-    }
+		
+		Enum::TestEnum();
+		Enum::TestEnumBitwidth();
+	}
 }
