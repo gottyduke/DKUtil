cc0d3ed65e20da41b701eeba2501addead36ec4b
vcpkg-action <gottyduke@hotmail.com>
2023-09-21 02:25:19 +0800

    feat: refactor Hook `FuncInfo`

--- include/DKUtil/Impl/Hook/JIT.hpp
+++ include/DKUtil/Impl/Hook/JIT.hpp
@@ -4,70 +4,49 @@
 #include "assembly.hpp"
 
 
-#define FUNC_INFO(FUNC)                               \
-	DKUtil::Hook::FuncInfo                            \
-	{                                                 \
-		reinterpret_cast<std::uintptr_t>(FUNC), #FUNC \
-	}
-#define RT_INFO(FUNC, NAME) \
-	DKUtil::Hook::FuncInfo  \
-	{                       \
-		FUNC, NAME          \
+#define FUNC_INFO(FUNC)                           \
+	DKUtil::Hook::FuncInfo                        \
+	{                                             \
+		reinterpret_cast<std::uintptr_t>(FUNC),   \
+			DKUtil::Hook::GetFuncArgsCount(FUNC), \
+			#FUNC                                 \
 	}
 
 
 namespace DKUtil::Hook
 {
-	namespace JIT
+	template <typename Func, typename... Args>
+	inline consteval std::size_t GetFuncArgsCount(Func (*)(Args...))
 	{
-		class JitActionExpr : Xbyak::CodeGenerator
-		{
-		public:
-			JitActionExpr(Assembly::Register reg)
-			{
-			}
+		return decltype(std::integral_constant<unsigned, sizeof...(Args)>{})::value;
+	}
 
-			JitActionExpr(const JitActionExpr&){};
-			JitActionExpr(JitActionExpr&&) noexcept {};
-
-		private:
-		};
-	}  // namespace JIT
+	template <typename Func, class Class, typename... Args>
+	inline consteval std::size_t GetMemFuncArgsCount(Func (Class::*)(Args...))
+	{
+		return decltype(std::integral_constant<unsigned, sizeof...(Args)>{})::value;
+	}
 
 
 	class FuncInfo
 	{
 	public:
-		explicit constexpr FuncInfo(std::uintptr_t a_addr, std::string_view a_name) :
-			_address(a_addr), _name(a_name)
-		{
-			_argCount = 0;
-			_stackbase = 0;
-			_stackframe = 0;
-			std::fill_n(_buf.data(), _buf.size(), Assembly::NOP);
-		}
+		explicit constexpr FuncInfo(
+			std::uintptr_t a_addr,
+			std::uint8_t a_argsCount,
+			std::string_view a_name) :
+			_address(a_addr),
+			_argsCount(a_argsCount),
+			_name(a_name)
+		{}
 
-		FuncInfo& operator[](const std::constructible_from<JIT::JitActionExpr> auto&... expr) {}
-
-		// getter
 		[[nodiscard]] constexpr auto address() const noexcept { return _address; }
 		[[nodiscard]] constexpr auto name() const noexcept { return _name; }
-		[[nodiscard]] constexpr auto arg_count() const noexcept { return _argCount; }
-		[[nodiscard]] constexpr auto stackbase() const noexcept { return _stackbase; }
-		[[nodiscard]] constexpr auto stackframe() const noexcept { return _stackframe; }
-		[[nodiscard]] constexpr auto& buffer() const noexcept { return _buf; }
-		[[nodiscard]] constexpr auto prolog() const noexcept { return _prolog; }
-		[[nodiscard]] constexpr auto epilog() const noexcept { return _epilog; }
+		[[nodiscard]] constexpr auto args_count() const noexcept { return _argsCount; }
 
 	private:
 		const std::uintptr_t _address;
+		const std::uint8_t _argsCount;
 		const std::string_view _name;
-		std::uint8_t _argCount;
-		std::vector<JIT::JitActionExpr> _argExpr;
-		std::uintptr_t _stackbase;
-		std::uintptr_t _stackframe;
-		std::array<OpCode, CAVE_BUF_SIZE> _buf;
-		std::span<OpCode> _prolog;
-		std::span<OpCode> _epilog;
 	};
 }  // namespace DKUtil::Hook
