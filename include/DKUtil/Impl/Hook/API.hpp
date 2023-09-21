82d5899ce34805282c28d11a65a77e34be844581
vcpkg-action <gottyduke@hotmail.com>
2023-09-22 02:30:29 +0800

    feat: convert `RelHook` into `HookHandle`

--- include/DKUtil/Impl/Hook/API.hpp
+++ include/DKUtil/Impl/Hook/API.hpp
@@ -7,6 +7,13 @@
 
 namespace DKUtil::Hook
 {
+	/* @brief Apply assembly patch in the body of execution
+	 * @param a_address : Memory address of the BEGINNING of target function
+	 * @param a_offset : Offset pairs for <beginning, end> of cave entry from the head of function
+	 * @param a_patch : Assembly patch
+	 * @param a_forward : Skip the rest of NOPs until next valid opcode
+	 * @returns ASMPatchHandle
+	 */
 	inline auto AddASMPatch(
 		const std::uintptr_t        a_address,
 		const offset_pair           a_offset,
@@ -16,6 +23,13 @@ namespace DKUtil::Hook
 		return AddASMPatch(a_address, a_offset, std::make_pair(a_xbyak->getCode(), a_xbyak->getSize()), a_forward);
 	}
 
+	/* @brief Apply assembly patch in the body of execution
+	 * @param a_address : Memory address of the BEGINNING of target function
+	 * @param a_offset : Offset pairs for <beginning, end> of cave entry from the head of function
+	 * @param a_patch : Assembly patch
+	 * @param a_forward : Skip the rest of NOPs until next valid opcode
+	 * @returns ASMPatchHandle
+	 */
 	inline auto AddASMPatch(
 		const std::uintptr_t a_address,
 		const offset_pair    a_offset,
@@ -25,6 +39,15 @@ namespace DKUtil::Hook
 		return AddASMPatch(a_address, a_offset, std::make_pair(a_patch->Data, a_patch->Size), a_forward);
 	}
 
+	/* @brief Branch to hook function in the body of execution from target function.
+	 * @param a_offset : Offset pairs for <beginning, end> of cave entry from the head of function
+	 * @param a_address : Memory address of the BEGINNING of target function
+	 * @param a_funcInfo : FUNC_INFO or RT_INFO wrapper of hook function
+	 * @param a_prolog : Prolog patch before detouring to hook function
+	 * @param a_epilog : Epilog patch after returning from hook function
+	 * @param a_flag : Specifies operation on cave hook
+	 * @returns CaveHookHandle
+	 */
 	inline auto AddCaveHook(
 		const std::uintptr_t         a_address,
 		const offset_pair            a_offset,
@@ -40,6 +63,15 @@ namespace DKUtil::Hook
 			a_flag);
 	}
 
+	/* @brief Branch to hook function in the body of execution from target function.
+	 * @param a_offset : Offset pairs for <beginning, end> of cave entry from the head of function
+	 * @param a_address : Memory address of the BEGINNING of target function
+	 * @param a_funcInfo : FUNC_INFO or RT_INFO wrapper of hook function
+	 * @param a_prolog : Prolog patch before detouring to hook function
+	 * @param a_epilog : Epilog patch after returning from hook function
+	 * @param a_flag : Specifies operation on cave hook
+	 * @returns CaveHookHandle
+	 */
 	inline auto AddCaveHook(
 		const std::uintptr_t         a_address,
 		const offset_pair            a_offset,
@@ -93,15 +125,38 @@ namespace DKUtil::Hook
 		return AddIATHook(a_moduleName, a_libraryName, a_importName, a_funcInfo, std::make_pair(a_patch->Data, a_patch->Size));
 	}
 
-	template <std::size_t N, class F>
-	inline auto write_branch(std::uintptr_t a_src, F a_dst)
+	/* @brief Relocate a jmpsite with target hook function
+	 * @brief This API exists for compatiblity reason with CLib-style invocations, hook enabled by default
+	 * @param <N> : Length of source instruction
+	 * @param a_src : Address of jmp instruction
+	 * @param a_dst : Destination function
+	 * @returns Transitive RelHookHandle that can be converted to F
+	 */
+	template <std::size_t N, typename F>
+	inline auto write_branch(
+		std::uintptr_t a_src, 
+		F a_dst) noexcept
 	{
-		return AddRelHook<N, false>(a_src, unrestricted_cast<std::uintptr_t>(a_dst));
+		auto handle = write_branch_ex<N>(a_src, a_dst);
+		handle->Enable();
+		return std::move(*handle.get());
 	}
 
-	template <std::size_t N, class F>
-	inline auto write_call(std::uintptr_t a_src, F a_dst)
+	/* @brief Relocate a callsite with target hook function
+	 * @brief This API exists for compatiblity reason with CLib-style invocations, hook enabled by default
+	 * @param <N> : Length of source instruction
+	 * @param a_src : Address of call instruction
+	 * @param a_dst : Destination function
+	 * @returns Transitive RelHookHandle that can be converted to F
+	 */
+	template <std::size_t N = 5, typename F>
+		requires(model::concepts::dku_memory<F>)
+	inline auto write_call(
+		std::uintptr_t a_src, 
+		F a_dst) noexcept
 	{
-		return AddRelHook<N, true>(a_src, unrestricted_cast<std::uintptr_t>(a_dst));
+		auto handle = AddRelHook<N, true>(a_src, unrestricted_cast<std::uintptr_t>(a_dst));
+		handle->Enable();
+		return std::move(*handle.get());
 	}
 }  // namespace DKUtil::Hook
